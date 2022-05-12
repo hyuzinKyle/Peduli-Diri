3bac258e26f84b91eef662ca83e93ba1171323d8
M Libran <xirpl2.mlibranrestuwibawa@gmail.com>
2022-05-12 10:19:39 +0700

    Add Close Button On Login Alert

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -41,6 +41,7 @@
                         @csrf
                         @if (session('message'))
                             <div class="alert">
+                                <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
                                 {{ session('message') }}
                             </div>
                         @endif
